176ca12fe3ad4ea1a19749e58cc07b9bd02f4f09
rmangra <richard.mangra@gmail.com>
2018-01-01 23:00:06 -0500

    Worked on some MVC Fundamentals.

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -12,8 +12,13 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var movie = new Movie() {Name = "Shrek!"};
-            return View(movie);
+            var movie = new Movie() { Name = "Shrek!" };
+
+            // return View(movie);
+            // return Content("Hello World!");
+            // return HttpNotFound();
+            // return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name" });
         }
     }
 }
