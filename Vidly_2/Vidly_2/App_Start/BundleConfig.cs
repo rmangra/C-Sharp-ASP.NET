8116f0af04666ad074d06768b77e36cd3e1970fe
rmangra <richard.mangra@gmail.com>
2018-01-01 23:53:16 -0500

    Adding a theme.

--- Vidly_2/Vidly_2/App_Start/BundleConfig.cs
+++ Vidly_2/Vidly_2/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Vidly_2
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
