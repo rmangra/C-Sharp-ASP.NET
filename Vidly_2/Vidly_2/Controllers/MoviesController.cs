9f9067ab2fa0b38e3c4824d220115b3e7ccf9c64
rmangra <richard.mangra@gmail.com>
2018-01-02 01:33:12 -0500

    Added action parameters.

--- Vidly_2/Vidly_2/Controllers/MoviesController.cs
+++ Vidly_2/Vidly_2/Controllers/MoviesController.cs
@@ -15,6 +15,28 @@ namespace Vidly_2.Controllers
             var movie = new Movie() { Name = "Shrek!" };
 
             return View(movie);
+            // return Content("Hello World!");
+            // return HttpNotFound();
+            // return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+                      
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
