c082b418785347a1ae5994238c0a959bde6ad709
rmangra <richard.mangra@gmail.com>
2018-01-01 23:40:20 -0500

    Completed MVC in Action exercise.

---
+++ Vidly_2/Vidly_2/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly_2.Models;
+
+namespace Vidly_2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
