042dbfad1044e4fd5c7829693f706814d30e9aee
rmangra <richard.mangra@gmail.com>
2018-01-12 14:15:56 -0500

    Completed exercise to implement features for the Vidly app.

--- Vidly_2/Vidly_2/Controllers/MoviesController.cs
+++ Vidly_2/Vidly_2/Controllers/MoviesController.cs
@@ -10,14 +10,30 @@ namespace Vidly_2.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
                 new Customer { Name = "Customer 1" },
-                new Customer { Name = "Customer 2"}
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
@@ -27,35 +43,6 @@ namespace Vidly_2.Controllers
             };
 
             return View(viewModel);
-            // return Content("Hello World!");
-            // return HttpNotFound();
-            // return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
-                      
         }
-
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
-
-        /*public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-        */
     }
 }
